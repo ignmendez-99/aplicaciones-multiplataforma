5517ded4a431a21fa3ac6491580d6ebdbbcc6103
Ignacio <isampedro@itba.edu.ar>
2023-04-19 16:03:20 -0300

    Estructura pagina welcome

---
+++ lib/welcome.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class Welcome extends StatelessWidget {
+  const Welcome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: null,
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Colors.white,
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/logo_cuadrado.png',
+                height: 150,
+                width: 150,
+              ),
+              const SizedBox(height: 20),
+              const Center(
+                child: Text(
+                    '¡Bienvenido!',
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                    textAlign: TextAlign.center
+                ),
+              ),
+              const SizedBox(height: 50),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/home');
+                  },
+                  child: const Text('Comenzar'),
+                ),
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
